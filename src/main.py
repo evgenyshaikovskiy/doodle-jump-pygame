7f547f251bc81dc35d094ab25dd794b2ac2f47a7
Yauheni Shaikouski <evgenyshaikovskiy@gmail.com>
2022-04-28 09:34:58 +0300

    update solution

--- src/main.py
+++ src/main.py
@@ -1,10 +1,19 @@
-import pygame
+import json
+from game.jumper import DoodleJump
 
 
 # application entry point
 def main():
-    pygame.init()
+    game_settings = parse_settings()
+    DoodleJump(game_settings).run()
 
 
-if __name__ == 'main':
+def parse_settings():
+    with open('src/utility/settings.json') as sgs:
+        settings = json.load(sgs)
+
+    return settings
+
+
+if __name__ == '__main':
     main()
