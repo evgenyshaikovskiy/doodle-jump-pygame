dc57060aebdd597053aacdf2e2062c4fa6958bc8
Yauheni Shaikouski <evgenyshaikovskiy@gmail.com>
2022-05-02 22:05:26 +0300

    refactor settings logic

--- src/utility/loader.py
+++ src/utility/loader.py
@@ -1,16 +1,18 @@
 import json
 
-
-settings = None
-
-
-def get_settings(key: str):
-    if settings is None:
-        __parse_settings__()
-
-    return settings[key]
+# there is a problem with settings
+# each time we get setting, we read file again
 
 
 def __parse_settings__():
     with open('src/utility/settings.json') as sgs_file:
         settings = json.load(sgs_file)
+
+    return settings
+
+
+settings = __parse_settings__()
+
+
+def get_setting(key: str):
+    return settings[key]
