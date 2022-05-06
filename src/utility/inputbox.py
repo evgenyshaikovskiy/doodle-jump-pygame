d82280220ff947057eda4de90250edffadffc9b8
Yauheni Shaikouski <evgenyshaikovskiy@gmail.com>
2022-05-06 18:49:35 +0300

    update inputbox

--- src/utility/inputbox.py
+++ src/utility/inputbox.py
@@ -2,21 +2,12 @@ import pygame
 from pygame.locals import *
 
 
-def get_key():
-    while True:
-        event = pygame.event.poll()
-        if event.type == pygame.KEYDOWN:
-            return event.key
-        else:
-            pass
-
-
 def display_box(screen, message):
     fontobject = pygame.font.Font(None, 18)
 
     pygame.draw.rect(
         screen,
-        (0, 0, 0),
+        (255, 0, 0),
         (
             (screen.get_width() / 2) - 100,
             (screen.get_height() / 2) - 10,
@@ -49,22 +40,21 @@ def display_box(screen, message):
 
 def ask(screen, question):
     pygame.font.init()
-    current_string = []
+    text: str = ''
+    display_box(screen, question + ':' + text)
+    typing = True
+    while typing:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                print(event.key)
+                print(int(event.key))
+                if event.key == pygame.K_RETURN:
+                    typing = False
+                elif event.key == pygame.K_BACKSPACE:
+                    text = text[:-1]
+                else:
+                    text += event.unicode
 
-    display_box(screen, question + ': ' + ''.join(current_string))
-    while True:
-        token = get_key()
-        if token == pygame.K_BACKSPACE:
-            current_string = current_string[0: -1]
-        elif token == pygame.K_RETURN:
-            break
-        elif token == pygame.K_MINUS:
-            current_string.append("_")
-        elif token == pygame.K_ESCAPE:
-            return None
-        elif token <= 127:
-            current_string.append(str(token))
+        display_box(screen, question + ':' + text)
 
-        display_box(screen, question + ': ' + str(''.join(current_string)))
-
-    return ''.join(current_string)
+    return text
